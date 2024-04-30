13603b986fbac2f29bdd521ad523304e97f9b76e
ivanalineriPoatek <ivan.alineri@poatek.com>
2024-04-30 18:46:11 -0300

    Topbar component

--- app/src/main/java/com/example/cajucopy/ui/TopBarComposable.kt
+++ app/src/main/java/com/example/cajucopy/ui/TopBarComposable.kt
@@ -1,7 +1,10 @@
 package com.example.cajucopy.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -9,11 +12,14 @@ import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cajucopy.R
 
@@ -22,21 +28,54 @@ import com.example.cajucopy.R
 fun TopBarComponent() {
     TopAppBar(
         modifier = Modifier.padding(12.dp),
-        title = { Text(text = "Oi, Rafel") },
+        title = { textBold(name = "Rafael") },
         navigationIcon = {
-            Icon(
-                modifier = Modifier
-                    .clip(shape = CircleShape)
-                    .background(Color.Gray),
-                painter = painterResource(id = R.drawable.baseline_person_outline_24),
-                contentDescription = ""
-            )
+            SetIconShape()
         },
         actions = {
             Icon(
+                modifier = Modifier.padding(end = 12.dp),
                 painter = painterResource(id = R.drawable.baseline_visibility_24),
                 contentDescription = ""
             )
         }
     )
+}
+
+@Composable
+fun textBold(name: String): String {
+    Row(modifier = Modifier.padding(12.dp)) {
+        Text(text = "Oi, ")
+        Text(
+            text = name,
+            fontWeight = FontWeight.Bold
+        )
+    }
+    return name
+}
+
+@Composable
+fun SetIconShape() {
+    Box(contentAlignment = Alignment.CenterStart,
+        modifier = Modifier
+            .size(50.dp)
+            .clip(CircleShape)
+            .background(Color.Gray)
+
+    ) {
+        Icon(
+            modifier = Modifier
+                .padding(start = 12.dp),
+            painter = painterResource(id = R.drawable.baseline_person_outline_24),
+            contentDescription = ""
+        )
+    }
+
+}
+
+@Preview
+@Composable
+private fun ToolBarComponentPreview() {
+    TopBarComponent()
+
 }
